fece443e385b024f0faf21bf46241101568d74e9
tehies <kulwinder.kumar@iamtechie.com>
2025-06-23 15:49:03 +0530

    latest commit

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,9 +1,20 @@
-import { prisma } from '@/db/prisma';
+import { PrismaClient } from "@prisma/client";
 
+
+//Get latest Products
 export async function getLatestProduct() {
+const prisma = new PrismaClient()
   const data = await prisma.product.findMany({
     take: 4,
     orderBy: { createdAt: 'desc' },
   });
   return data;
+}
+
+//Get single Products
+export async function getProductBySlug(slug: string) {
+  const prisma = new PrismaClient()  
+  return await prisma.product.findFirst({
+    where: {slug: slug},
+  });
 }
