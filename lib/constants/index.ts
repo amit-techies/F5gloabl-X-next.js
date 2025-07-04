42211521bfecd2afee1829181b2dfc0ce972a30f
tehies <kulwinder.kumar@iamtechie.com>
2025-07-04 18:42:14 +0530

    payment

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -16,9 +16,12 @@ export const signUpDefaultValues= {
 };
 
 export const shippingAddressDefaultValues = {
-    fullName: '',
-    streetAddress: '',
-    city: '',
-    postalCode: '',
-    country: '',
+    fullName: 'Kulwinder',
+    streetAddress: '8b Industrial area',
+    city: 'Mohali',
+    postalCode: '160055',
+    country: 'India',
 }
+
+export const PAYMENT_METHODS =process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(', ') : ['PayPal', 'Stripe', 'CashOnDelivery'];
+export const DEFAULT_PAYMENT_METHOD=process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
