74a01b5e15016b54dee3394e0f299e5f0bc3fd93
Amit-techies <amit.singh@iamtechie.com>
2025-07-25 12:09:45 +0530

    auth fixes

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,5 +1,19 @@
-import { DefaultSelection } from "next-auth";
+import { DefaultSession } from 'next-auth';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    sub: string;
+    role: string;
+    name: string;
+  }
+}
 
 declare module 'next-auth' {
   /**
